73ca09eeef65786686680831cb8db42872d657d9
jackrk <kohn.jack@yahoo.com>
2014-11-06 16:36:11 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res) {
+	res.render('home');
+});
+
+router.get('/profile', function(req, res) {
+	res.render('profile');
+});
+
+module.exports = router;
